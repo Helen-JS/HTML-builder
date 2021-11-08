30c373bf2a9e0701b94fb975cea06603bde5ba22
Elena Samoilova <helen-js@mail.ru>
2021-11-08 15:36:22 -0700

    feat: complete files-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,20 +1,21 @@
 
-const {path} = require('path');
+const {resolve, extname, basename} = require('path');
 const {readdir, stat} = require('fs').promises;
 
 async function* traverseDir(root) {
-  console.log('Root: ' + root);
-  const fileNames = await readdir(root);
+  const fileNames = await readdir(root,{ withFileTypes: true });
   for (let fileName of fileNames) {
-    let filePath = path(root, fileName);
-    if ((await stat(filePath)).isDirectory()) {
+    const filePath = resolve(root, fileName.name);
+    if (fileName.isDirectory()) {
       yield* traverseDir(filePath);
     } else {
-      const size = (await stat(fileName)).size;
-      const extent = filePath.extname;
-      yield {...fileName, size, extent};
+      let extent = extname(fileName.name);
+      const name = basename(filePath, extent)
+      const size = (await stat(filePath)).size / 1024;
+      extent = extent.replace('.','');
+      yield {name, size, extent};
     }
   }
 }
 
-(async () => {for await (const {name, size, extent} of traverseDir('/')) console.log(name, size, extent);})();
+(async () => {for await (const {name, size, extent} of traverseDir('./03-files-in-folder/secret-folder')) console.log(`${name} - ${extent} - ${size}kb`);})();
