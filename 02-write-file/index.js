64322fc0999cf86397320bc8975c6302598f4503
Elena Samoilova <helen-js@mail.ru>
2021-11-09 09:59:21 -0700

    feat: refactor some logic, adjust the requirements to apply for the need of the task.

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,20 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+const {createWriteStream} = require('fs');
+const {resolve} = require('path');
 
-fs.writeFile(
-  path.join(__dirname, 'text.txt'),
-  'Всем привет!',
-  (err) => {
-    if (err) throw err;
-    console.log('Файл был создан');
-  }
-);
+let fileStream = createWriteStream(resolve(__dirname, 'text.txt'), {flags: 'a'});
+let input = process.openStdin();
+console.log("Welcome to writing program. " +
+    "\nIf you need some help please type .help." +
+    "\nProvide your input that you want to save into the file:");
+input.addListener('data', d => {
 
-fs.appendFile(
-  path.join(__dirname, 'text.txt'),
-  'Удачной проверки!',
-  err => {
-    if (err) throw err;
-    console.log('Файл был изменен');
-  }
-);
+    if(d.toString().trim() === '.exit' || d.toString().trim() === '.quit'){
+       input.end(()=> {
+          console.log("The program will exit soon. Thank you for using our application.");
+          fileStream.close();
+          process.exit();
+       });
+    } else if(d.toString().trim() === '.help'){
+        console.log("Here are some commands that acceptable by the application:\n" +
+            "\t\t .help - Display the available commands into the console\n" +
+            "\t\t .exit or .quit - Terminates the application. \n" +
+            "Please enter your input to save information into the file: \n");
+    } else {
+        fileStream.write(d.toString());
+    }
+})
